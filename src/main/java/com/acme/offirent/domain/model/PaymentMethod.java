345bbf3f6485d49ac1d3b28e7bc891a9881647e2
cledmir <56932765+cledmir@users.noreply.github.com>
2020-11-10 22:55:07 -0500

    develop

---
+++ src/main/java/com/acme/offirent/domain/model/PaymentMethod.java
@@ -0,0 +1,36 @@
+package com.acme.offirent.domain.model;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+
+import javax.persistence.*;
+import java.util.List;
+@Entity
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="payment_methods")
+public class PaymentMethod{
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private String cardNumber;
+
+    @NotNull
+    private String ownerName;
+
+    @NotNull
+    private String dueDate;
+
+    @NotNull
+    private String cv;
+
+    
+}
---
+++ src/main/java/com/acme/offirent/domain/service/OfficeService.java
@@ -0,0 +1,22 @@
+package com.acme.offirent.domain.service;
+
+import com.acme.offirent.domain.model.Office;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+public interface OfficeService {
+    Page<Office> getAllOffices(Pageable pageable);
+    Office getOfficeById  (Long officeId);
+    Page<Office> getAllOfficesByDistrictId(Long districtId, Pageable pageable);
+    Page<Office> getAllOfficesByPriceLessThanEqual (float price, Pageable pageable);
+    Page<Office> getAllOfficesByAccountId(Long accountId, Pageable pageable);
+    Page<Office> getAllOfficesByPriceLessThanEqualAndPriceGreaterThanEqual(float price1, float price2, Pageable pageable);
+    Office createOffice(Office office, Long accountId);
+    Office rateOffice(Long officeId, Office officeRequest);
+    Office updateOffice(Long officeId,Office officeRequest);
+    ResponseEntity<?> deleteOffice(Long officeId);
+
+}
